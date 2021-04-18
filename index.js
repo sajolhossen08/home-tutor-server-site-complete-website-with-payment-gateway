dfe96fcb1ea455594bfa73719fc14e268d1ed605
Shozol Hossen <khansajol08@gmail.com>
2021-04-18 22:20:39 +0600

    done

--- index.js
+++ index.js
@@ -42,10 +42,8 @@ client.connect(err => {
 
 app.get('/admin', (req, res)=>{
     const queryEmail = req.query.email;
-    // console.log(queryEmail);
     adminCollection.find({email: queryEmail})
     .toArray((err, documents) =>{
-        // console.log(documents);
         res.send(documents);
     });
 })
@@ -59,56 +57,27 @@ app.get('/clients', (req, res) =>{
 
 app.patch('/clients/:id', (req, res) =>{
     const id = req.params.id;
-    // console.log(req.body.action,req.body.actionBG, req.body.actionColor)
-    // console.log(id);
+
     clientCollection.updateOne({_id: ObjectId(id)},
     {
         $set: {action: req.body.action,actionBG: req.body.actionBG, actionColor: req.body.actionColor}
     })
     .then(result => {
-        // console.log("result paisi...\n",result.modifiedCount>0);
         res.send(result.modifiedCount > 0);
     })
 })
 
 app.post('/addServices', (req, res)=>{
-    const adminEmail = req.body.adminEmail;
-    const title = req.body.title;  
-    const description = req.body.description;
-    const file = req.files.file;
-    console.log(file);
-
-    const filePath = `${__dirname}/addedServices/${file.name}`;
-        file.mv(filePath, (err) => {
-            if(err){
-                console.log(err);
-                res.status(500).send({msg: 'Failed to upload image'});
-            }
-            const newImg = fs.readFileSync(filePath);
-            const encImg = newImg.toString('base64');
-
-            const image = {
-                contentType: file.mimetype,
-                size: file.size,
-                img: Buffer(encImg, 'base64')
-            };
-
-            serviceCollection.insertOne({adminEmail,title,description,image})
-            .then(result => {
-                fs.remove(filePath, error => {
-                if(error){
-                    console.log(error);
-                }
-                res.send(result.insertedCount > 0);
-            })
-        })
+    const comment = req.body;
+    serviceCollection.insertOne(comment)
+    .then(result => {
+        res.send(result.insertedCount > 0);
     })
 })
 
 app.get('/services', (req, res) =>{
     serviceCollection.find({})
     .toArray( (err, documents) =>{
-        // console.log(documents);
         res.send(documents); 
     })
 })
@@ -133,8 +102,7 @@ app.post('/addClientProject', (req, res)=>{
     const projectTitle = req.body.projectTitle;  
     const projectDetails = req.body.projectDetails;
     const file = req.files.file;
-    
-    // console.log(action,actionBG,actionColor,name,email,projectTitle,projectDetails,price,file);
+
     const filePath = `${__dirname}/clientProjects/${file.name}`;
         file.mv(filePath, (err) => {
             if(err){
@@ -164,10 +132,8 @@ app.post('/addClientProject', (req, res)=>{
 
 app.get('/clients/email', (req, res) =>{
     const queryEmail = req.query.checkedEmail;
-    // console.log(queryEmail);
     clientCollection.find({customerEmail: queryEmail})
     .toArray( (err, documents) =>{
-    // console.log(documents);
     res.send(documents); 
     })
 })
